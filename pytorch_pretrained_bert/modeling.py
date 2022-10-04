711382932ea83bab46b56d71a9cfcbab9d941b5f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 19:41:02 +0000

    Adding numeric_owner as keyword arguement

--- pytorch_pretrained_bert/modeling.py
+++ pytorch_pretrained_bert/modeling.py
@@ -508,7 +508,7 @@ class PreTrainedBertModel(nn.Module):
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(archive, tempdir)
@@ -616,7 +616,7 @@ class PreTrainedBertModel(nn.Module):
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(archive, tempdir)
